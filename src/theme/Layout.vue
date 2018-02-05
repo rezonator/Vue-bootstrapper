2822c7c03d21a4aed1f39da8a2c90c94a3bf2e39
jaroosh <jarek.wawro@gmail.com>
2018-02-05 18:30:30 +0100

    Bulma added and sass compilers'

--- src/theme/Layout.vue
+++ src/theme/Layout.vue
@@ -1,11 +1,30 @@
 <template>
-    <div>
-      <nav class="nav has-shadows">
-        <h1>DUPA</h1>
-      </nav>
+   <div>
+    <nav class="nav has-shadow">
+    <div class="container">
+      <a href="/">
+        <img src="http://bit.ly/vue-img" alt="Vue SPA" />
+      </a>
     </div>
+  </nav>
+    <section class="main-section section">
+      <div class="container content">
+
+      </div>
+    </section>
+   <footer class="footer">
+    <div class="container">
+      <div class="content has-text-centered">
+        Follow us on
+        <a href="https://twitter.com/bstavroulakis" target="_blank">Twitter</a>
+      </div>
+    </div>
+  </footer>
+  </div>
 </template>
 <script>
   export default {}
 </script>
-<style></style>
+<style lang="scss">
+  @import "~bulma";
+</style>
